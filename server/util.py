a12dea74c86d9f4ed1e2eaf755cb629aa885cddb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 10:22:29 +0200

    Creating face recognation app (server part)

--- server/util.py
+++ server/util.py
@@ -10,6 +10,14 @@ def cropp_image_if_2_eyes(image_path, image_base64_data):
     eye_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + 'haarcascade_eye.xml'
     )
+
+    if image_path:
+        img = cv2.imread(image_path)
+    else:
+        img = get_cv2_image_from_base64_string(image_base64_data)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = 
 def get_b64_test_image_for_virat():
     with open("b64.txt") as f:
         return f.read()
