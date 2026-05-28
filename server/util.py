05da5912a67724b6067a15215d76e0f9ff4c881f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 10:20:30 +0200

    Creating face recognation app (server part)

--- server/util.py
+++ server/util.py
@@ -7,6 +7,9 @@ def cropp_image_if_2_eyes(image_path, image_base64_data):
     face_cascade = cv2.CascadeClassifier(
          cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     )
+    eye_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_eye.xml'
+    )
 def get_b64_test_image_for_virat():
     with open("b64.txt") as f:
         return f.read()
