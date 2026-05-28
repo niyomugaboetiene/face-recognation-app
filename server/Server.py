39dc33e720cf48c176f224a856033a1d5d4c0714
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 09:20:42 +0200

    Creating face recognation app (server part)

--- server/Server.py
+++ server/Server.py
@@ -2,9 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "Hi"
+# @app.route('/hello')
+# def hello():
+#     return "Hi"
+@app.route('/classify_image', methoods = ['GET', 'POST'])
+def classify_image():
+    pass
 
 if __name__ == "__main__":
     app.run(port=5000)
