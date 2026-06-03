ac9eed4089de2d9aa3f4e9a3768120522c42d43b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-03 09:55:13 +0200

    building server of face recognation app

--- server/util.py
+++ server/util.py
@@ -12,7 +12,7 @@ def classify_image(image_base64_data, file_path=None):
         scalled_raw_har = cv2.resize(img_har, (32, 32))
         combined_img = np.vstack((scalled_raw_img.reshape(32 * 32 * 3, 1), scalled_raw_har.reshape(32 * 32, 1)))
 
-        len_image_array = 32 * 
+        len_image_array = 32 * 32 * 3 + 32 * 32
 
 
 def get_cv2_image_from_base64_string(b64str):
