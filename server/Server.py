39d0e773c17b2fca8400acb1ec85be376229855c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-10 10:26:53 +0200

    commit of face recognation app

--- server/Server.py
+++ server/Server.py
@@ -10,7 +10,11 @@ app = Flask(__name__)
 def classify_image():
     image_data = request.form['image_data']
 
-    util.classify_image(image_data)
+    response = jsonify(util.classify_image(image_data))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response
+
 
 if __name__ == "__main__":
     app.run(port=5000)
