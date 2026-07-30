8046bea0e50a9bf00e58fa2d249c6db022f007a8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-07-30 08:19:10 +0200

    Updates my face-recognation-app

---
+++ client/src/Home.jsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom"
+
+const Home = () => {
+    
+    return (
+        <div>
+            <div>
+                <div>
+                    <nav>
+
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
+}
