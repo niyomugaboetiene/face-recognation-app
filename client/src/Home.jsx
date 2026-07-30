8ab4027f7b8659cbd52051ec335efa7479d9018e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-07-30 09:21:46 +0200

    Updates my face-recognation-app

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -9,6 +9,7 @@ export default function Header() {
 {/* effects */}
       <div className="absolute top-[-20%] left-[-10%] w-[600px] h-[600px] bg-purple-600/40 rounded-full blur-[120px] pointer-events-none"></div>
       <div className="absolute bottom-[-20%] right-[-10%] w-[500px] h-[500px] bg-pink-600/30 rounded-full blur-[120px] pointer-events-none"></div>
+      <div className="absolute bottom-[60%] right-[-10%] w-[500px] h-[500px] bg-blue-500/30 rounded-full blur-[120px] pointer-events-none"></div>
       
       <div className="relative z-10 w-full">
         <div className="flex justify-between items-center p-5 max-w-7xl mx-auto px-8">
