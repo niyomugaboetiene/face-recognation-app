af8504afcb7d83d78be0fa77d53e8806d7a98c87
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-07-30 08:45:04 +0200

    Updates my face-recognation-app

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom"
 const Home = () => {
     
     return (
-        <div>
-            <div className="flex justify-between">
-                <div>
+        <div className="bg-black">
+            <div className="flex justify-between p-5">
+                <div className="text-white">
                     <h1>Logo</h1>
                 </div>
                 <div>
-                    <nav className="space-x-5 me-5 mt-2">
-                        <Link>About Us</Link>
-                        <Link>Solution</Link>
-                        <Link>Product</Link>
-                        <button>Login</button>
+                    <nav className="space-x-6 me-12 mt-2">
+                        <Link className="font-bold text-white text-lg hover:text-purple-500 transition-colors">About Us</Link>
+                        <Link className="font-bold text-white text-lg hover:text-purple-500 transition-colors">Solution</Link>
+                        <Link className="font-bold text-white text-lg hover:text-purple-500 transition-colors">Product</Link>
+                        <button className=" bg-[#9317a7] text-white font-bold py-2 px-8 rounded-full shadow-xl shadow-white hover:bg-[#ab12c2] transition-colors">Login</button>
                     </nav>
                 </div>
             </div>
